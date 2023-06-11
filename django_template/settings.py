d12ca408e050c50e1f15369938fdb3fef14b5aa7
pavelbaltov <pavel.baltov@gmail.com>
2023-06-11 14:09:46 +0200

    add: APPEND_SLASH setting

--- django_template/settings.py
+++ django_template/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH=False
