44a02522d03449e8022f315ea2cf40cc90df3e3a
pavelbaltov <pavel.baltov@gmail.com>
2023-06-14 14:34:29 +0200

    fix: get_players returns non-friend players

--- social_app/views.py
+++ social_app/views.py
@@ -70,7 +70,7 @@ def get_players(request):
     if not request.user.is_authenticated:
         return HttpResponse(f'User not signed in')
 
-    non_friend_players = [player for player in Player.objects.all() if player.is_friend_with(request.user.player)]
+    non_friend_players = [player for player in Player.objects.all() if not player.is_friend_with(request.user.player)]
     players = [
         {
             "id": player.user.id,
