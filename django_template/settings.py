0c265709677411b7ca88f35e85b9563de03f5d97
pavelbaltov <pavel.baltov@gmail.com>
2023-06-22 10:59:36 +0200

    edit: updated settings

--- django_template/settings.py
+++ django_template/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_app.middleware.DisableCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'django_template.urls'
