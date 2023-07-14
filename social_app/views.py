7650887dc465265491b8651e4020f6595aad9691
pavelbaltov <pavel.baltov@gmail.com>
2023-07-14 18:17:45 +0200

    fix: minor

--- social_app/views.py
+++ social_app/views.py
@@ -765,7 +765,8 @@ def clear_player(request):
     request.user.player.role = None
     match = Match.objects.filter(host=request.user.username)
     if match.exists():
-        match.delete()
+        if match.get().player_set.count() == 0:
+            match.get().delete()
 
     request.user.player.match = None
     request.user.player.ready = False
